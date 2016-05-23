e65dc7faa32c9623437dbb4e619e838d3084f6df
龙仕云 <mrlong.com@gmail.com>
2016-05-23 09:29:16 +0800

    增加bug列表显示权限的问题。
    Signed-off-by: 龙仕云 <mrlong.com@gmail.com>

--- controller/login.js
+++ controller/login.js
@@ -2,6 +2,11 @@ var express = require('express');
 var router = express.Router();
 var Db = require('../lib/db.js');
 
+//退出
+router.get('/logout',function(req,res,next){
+  req.session.user = null;
+  res.redirect('/login');
+});
 
 router.get('/',function(req,res,next){
   
@@ -23,7 +28,8 @@ router.post('/',function(req,res,next){
     var mysql = "select * from TB_USER_ITEM where ZNAME='" + myname +"' and ZPASS='"+ mypasswd +"'" ;
     Db.query(mysql,function(err,data){
       if(!err && data.length > 0 ){
-        req.session.user = {name:myname,ZID:data[0].ZID};
+        //类型=0系统用户,1=开发人员 2=测试人员,3=其他人员
+        req.session.user = {name:myname,ZID:data[0].ZID,ZTYPE:data[0].ZTYPE};
         res.redirect('/main');  
       } 
       else{
@@ -33,6 +39,8 @@ router.post('/',function(req,res,next){
     });
         
 });
+
+
 
 
 
--- controller/main.js
+++ controller/main.js
@@ -1,34 +1,83 @@
 var express = require('express');
 var router = express.Router();
 var Db = require('../lib/db.js');
+var EventProxy = require('eventproxy');
 
 
 router.get('/',function(req,res,next){
   
   var pageindex = req.query.page || 1;
-  var mysql;
-  if(pageindex==1){
-    mysql = 'select top 20 ZID,ZTITLE,ZRESOLVEDBY from TB_BUG_ITEM order by ZOPENEDDATE desc ';
-  }
-  else{
-    mysql = 'select TOP 20 ZID,ZTITLE,ZRESOLVEDBY from TB_BUG_ITEM where ZID not in(' + 
-              'select TOP ' + 20 * (pageindex -1) + ' ZID from TB_BUG_ITEM order by ZOPENEDDATE desc) order by ZOPENEDDATE desc ';
-  };
+  var curuserid = req.session.user.ZID;
   
-  Db.query(mysql,function(err,data){
-    if(!err){
-      Db.query('select count(*) as myv from TB_BUG_ITEM',function(err,count){
-        res.render('main.html',{bugitems:data,
-                                msgbox:req.flash('msgbox'),
-                                rowcount:!err? count[0].myv : 0 ,
-                                curpageindex:pageindex});  
-      });
-      
+  var curuserid = req.session.user.ZID;
+  var ep = new EventProxy();
+  
+
+  ep.all('right',function(right){
+    
+    var mysql;
+    
+    var mywhere='1=1';
+    //不是系统管理员时，则要列出bug的权限来。
+    if(req.session.user.ZTYPE !=0){
+      mywhere += ' and(';
+      for(var i=0;i<right.length;i++){
+        mywhere +=  (i>0?' or ':'') + 'ZTREE_ID=' + right[i].ZMODULEID; 
+      };
+      if(right.length==0){
+        mywhere += ' ZTREE_ID=9999 )'  
+      }
+      else{
+        mywhere += ')';
+      };
+    };
+    
+    if(pageindex==1){
+      mysql = 'select top 20 ZID,ZTITLE,ZRESOLVEDBY from TB_BUG_ITEM where ' + mywhere +' order by ZOPENEDDATE desc ';
     }
     else{
-      res.render('main.html',{bugitems:[]});     
-    }
+      mysql = 'select TOP 20 ZID,ZTITLE,ZRESOLVEDBY from TB_BUG_ITEM where ' + mywhere +' and  ZID not in(' + 
+                'select TOP ' + 20 * (pageindex -1) + ' ZID from TB_BUG_ITEM where ' + mywhere +' order by ZOPENEDDATE desc) order by ZOPENEDDATE desc ';
+    };
+
+    Db.query(mysql,function(err,data){
+      if(!err){
+        Db.query('select count(*) as myv from TB_BUG_ITEM where ' + mywhere ,function(err,count){
+          res.render('main.html',{bugitems:data,
+                                  msgbox:req.flash('msgbox'),
+                                  rowcount:!err? count[0].myv : 0 ,
+                                  curpageindex:pageindex});  
+        });
+
+      }
+      else{
+        res.render('main.html',{
+          bugitems:[],
+          msgbox:[{success:false,msg:'读取数据内的数据出错'+err}],
+          rowcount:0,
+          curpageindex:1});     
+      }
+    });
+    
+    
   });
+        
+  
+  //取出权限来
+  if(!req.session.user.bugright){
+    var mysqlright = 'select ZMODULEID from TB_USER_PRIVILEGE where ZSTYLE=200 and ZSUBSTYLE=1 and ZUSER_ID=' + curuserid;
+    Db.query(mysqlright,function(err,rows){
+      if(!err && rows){
+        req.session.user.bugright = rows;
+        ep.emit('right',rows);
+      }
+    });
+  }
+  else{
+    ep.emit('right',req.session.user.bugright);
+  };
+      
+  
   
     
 });
